9eedb1e773e1fe4c0f80d9b5269c6b444836a48d
Robert Staph <rstaph@gmail.com>
2018-08-22 11:17:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WaveshareEPaper29
---
+++ src/WaveshareEPaper29.h
@@ -0,0 +1,38 @@
+/**
+ *  @filename   :   WaveshareEPaper29.h
+ *  @brief      :   Dummy header file to upload Waveshare ep2in9 library to
+ *                  particle.io library repository
+ *  @author     :   Particle member rstaph
+ *
+ *  Copyright (C) rstaph     August 22 2018
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documnetation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to  whom the Software is
+ * furished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#pragma once
+
+#include <Particle.h> 
+#include "epdif.h"
+#include "epd2in9.h"
+#include "epdpaint.h"
+
+// *************************   ToDo   **************************
+// add software SPI support
+// replace epdpaint.h/.cpp for Adafruit_GFX/Adafruit_mfGFX
+// generalise for other Waveshare displays too
+// *************************************************************
